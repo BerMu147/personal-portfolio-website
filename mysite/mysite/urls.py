0244a08ac758041d4e3f04dddf02e47aa5f2d386
BerMu147 <berin.mujcinovic.98@gmail.com>
2024-02-09 14:34:49 +0100

    Resolve static files loading

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path("projects/", include("projects.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += staticfiles_urlpatterns()
