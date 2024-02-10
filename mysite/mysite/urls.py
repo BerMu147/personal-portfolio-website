9d417bf73983dc8ab51abfcc72fbc96670d4b7ce
BerMu147 <berin.mujcinovic.98@gmail.com>
2024-02-10 19:11:07 +0100

    Resolved media files loading

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path("", include("contact.urls")),
     path("projects/", include("projects.urls")),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
 urlpatterns += staticfiles_urlpatterns()
--- mysite/projects/views.py
+++ mysite/projects/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
 from projects.models import Project
 
 
