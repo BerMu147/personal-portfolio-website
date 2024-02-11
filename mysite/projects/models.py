1ca877623c8a8f5de3dad923924060e2d6d8dc0a
BerMu147 <berin.mujcinovic.98@gmail.com>
2024-02-11 03:59:57 +0100

    Improved project detail and style

--- mysite/projects/models.py
+++ mysite/projects/models.py
@@ -7,4 +7,5 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    link = models.URLField(blank=True)
     image = models.FileField(upload_to="project_images/", blank=True)
