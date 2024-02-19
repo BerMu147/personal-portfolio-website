57de211fe2babdff619df1dac247bde202fffccd
BerMu147 <berin.mujcinovic.98@gmail.com>
2024-02-19 18:39:35 +0100

    List previous experiences on page

--- mysite/experience/models.py
+++ mysite/experience/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Experience(models.Model):
     organizationName = models.CharField(max_length=100)
+    image = models.FileField(upload_to="experience_images/", blank=True)
     position = models.CharField(max_length=100)
     description = models.TextField()
     startDate = models.DateField()
--- mysite/experience/views.py
+++ mysite/experience/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from experience.models import Experience
 
-# def experience(request):
-#     return render(request, "experience/experience.html", context)
-
 def experience(request):
 
-    projects = Experience.objects.all()
+    experience = Experience.objects.all()
 
     context = {
         "experience": experience
